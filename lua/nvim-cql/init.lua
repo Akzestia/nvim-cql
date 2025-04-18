aa141318db944ac4fa9f6a2c52c29120c9b00daa
アクゼスティア <akzestia@gmail.com>
2025-04-19 01:19:54 +0200

    Trying to make highlights queries work ^^

--- lua/nvim-cql/init.lua
+++ lua/nvim-cql/init.lua
@@ -1,46 +1,31 @@
 local M = {}
 
-function M.setup(user_config)
-    local config = vim.tbl_deep_extend("force", {
-        treesitter = {
-            enable = true,
-            highlight = { enable = true },
-        }
-    }, user_config or {})
-
-    vim.filetype.add({ extension = { cql = "cql" } })
-    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-        pattern = "*.cql",
-        callback = function() vim.bo.filetype = "cql" end
-    })
-
+local function install_parser()
     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
     parser_config.cql = {
         install_info = {
             url = "https://github.com/Akzestia/tree-sitter-cql",
             files = { "src/parser.c" },
-            -- Add compiled parser location
-            location = vim.fn.stdpath("data") .. "/site/parser/cql.so",
             branch = "main",
+            generate_requires_npm = false,
+            requires_generate_from_grammar = false,
         },
         filetype = "cql",
     }
+end
 
-    if config.treesitter.enable then
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = "cql",
-            callback = function()
-                require("nvim-treesitter.configs").setup({
-                    highlight = {
-                        enable = true,
-                        additional_vim_regex_highlighting = false,
-                    },
-
-                })
-                vim.treesitter.start()
-            end
-        })
-    end
+function M.setup()
+    vim.filetype.add({ extension = { cql = "cql" } })
+    install_parser()
+    require("nvim-treesitter.configs").setup({
+        ensure_installed = { "cql" },
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        incremental_selection = { enable = true },
+        textobjects = { enable = true },
+    })
 end
 
 return M
